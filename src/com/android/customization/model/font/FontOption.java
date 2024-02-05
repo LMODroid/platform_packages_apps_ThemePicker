fe9843f93113a15ba0f45c36d77a0fd7fb291503
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-02-05 17:57:12 +0530

    ThemePicker: Update UI for custom sections
    Adapt font and icon pickers to the new colors in U-QPR1.
    
    Change-Id: Ibd65374e5badda79af085cfd4345ee925ed72482

--- src/com/android/customization/model/font/FontOption.java
+++ src/com/android/customization/model/font/FontOption.java
@@ -57,10 +57,10 @@ public class FontOption implements CustomizationOption<FontOption> {
         Resources res = view.getContext().getResources();
         ((TextView) view.findViewById(R.id.thumbnail_text)).setTypeface(
                 mHeadlineFont);
-        int colorFilter = ResourceUtils.getColorAttr(view.getContext(),
+        int colorFilter = view.getContext().getResources().getColor(
                 view.isActivated() || view.getId() == R.id.font_section_tile
-                        ? android.R.attr.textColorPrimary
-                        : android.R.attr.textColorTertiary);
+                        ? com.android.wallpaper.R.color.system_on_surface
+                        : com.android.wallpaper.R.color.system_on_surface_variant);
         ((TextView) view.findViewById(R.id.thumbnail_text)).setTextColor(colorFilter);
         view.setContentDescription(mTitle);
     }
