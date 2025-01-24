9cbcf72a03871c0a5b29539f39c24c47c689dea6
Michael Bestas <mkbestas@lineageos.org>
2025-01-24 15:46:16 +0200

    ThemePicker: Drop corner radius from icon shapes
    Change-Id: Ia715dee7e694682839731128b4b902a62410c1f5

--- src/com/android/customization/model/iconshape/IconShapeOption.java
+++ src/com/android/customization/model/iconshape/IconShapeOption.java
@@ -51,20 +51,17 @@ public class IconShapeOption implements CustomizationOption<IconShapeOption> {
     private final String mTitle;
     private final String mOverlayPackage;
     private final Path mPath;
-    private final int mCornerRadius;
     private int[] mShapeIconIds = {
             R.id.shape_preview_icon_0, R.id.shape_preview_icon_1, R.id.shape_preview_icon_2,
             R.id.shape_preview_icon_3, R.id.shape_preview_icon_4, R.id.shape_preview_icon_5
     };
 
     public IconShapeOption(String packageName, String title, Path path,
-                           @Dimension int cornerRadius, Drawable shapeDrawable,
-                           List<ShapeAppIcon> appIcons) {
+                           Drawable shapeDrawable, List<ShapeAppIcon> appIcons) {
         mOverlayPackage = packageName;
         mTitle = title;
         mAppIcons = appIcons;
         mPath = path;
-        mCornerRadius = cornerRadius;
         mShape = shapeDrawable.getConstantState().newDrawable();
     }
 
