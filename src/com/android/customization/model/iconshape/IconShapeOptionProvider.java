67144ef3940abcd660777c40e7793489f1b85d1a
Michael Bestas <mkbestas@lineageos.org>
2025-01-24 15:46:16 +0200

    ThemePicker: Drop corner radius from icon shapes
    Change-Id: Ia715dee7e694682839731128b4b902a62410c1f5

--- src/com/android/customization/model/iconshape/IconShapeOptionProvider.java
+++ src/com/android/customization/model/iconshape/IconShapeOptionProvider.java
@@ -16,7 +16,6 @@
 package com.android.customization.model.iconshape;
 
 import static com.android.customization.model.ResourceConstants.ANDROID_PACKAGE;
-import static com.android.customization.model.ResourceConstants.CONFIG_CORNERRADIUS;
 import static com.android.customization.model.ResourceConstants.CONFIG_ICON_MASK;
 import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY_SHAPE;
 import static com.android.customization.model.ResourceConstants.PATH_SIZE;
@@ -84,8 +83,7 @@ public class IconShapeOptionProvider {
                 PackageManager pm = mContext.getPackageManager();
                 String label = pm.getApplicationInfo(overlayPackage, 0).loadLabel(pm).toString();
                 mOptions.add(new IconShapeOption(overlayPackage, label, path,
-                        loadCornerRadius(overlayPackage), createShapeDrawable(path),
-                        getShapedAppIcons(path)));
+                        createShapeDrawable(path), getShapedAppIcons(path)));
             } catch (NameNotFoundException | NotFoundException e) {
                 Log.w(TAG, String.format("Couldn't load shape overlay %s, will skip it",
                         overlayPackage), e);
@@ -97,9 +95,6 @@ public class IconShapeOptionProvider {
         Resources system = Resources.getSystem();
         Path path = loadPath(system, ANDROID_PACKAGE);
         mOptions.add(new IconShapeOption(null, mContext.getString(R.string.default_theme_title), path,
-                system.getDimensionPixelOffset(
-                        system.getIdentifier(CONFIG_CORNERRADIUS,
-                                "dimen", ResourceConstants.ANDROID_PACKAGE)),
                 createShapeDrawable(path), getShapedAppIcons(path)));
     }
 
@@ -148,16 +143,4 @@ public class IconShapeOptionProvider {
         }
         return null;
     }
-
-    @Dimension
-    private int loadCornerRadius(String packageName)
-            throws NameNotFoundException, NotFoundException {
-
-        Resources overlayRes =
-                mContext.getPackageManager().getResourcesForApplication(
-                        packageName);
-        return overlayRes.getDimensionPixelOffset(overlayRes.getIdentifier(
-                CONFIG_CORNERRADIUS, "dimen", packageName));
-    }
-
 }
